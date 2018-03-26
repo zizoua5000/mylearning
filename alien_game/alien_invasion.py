2bf9a2d25cda0f93e13cfec31ed4c42934709785
zizoua5000 <zizoua5000@gmail.com>
2018-03-26 11:08:39 +0400

    my first python game dev process

---
+++ alien_game/alien_invasion.py
@@ -0,0 +1,17 @@
+import pygame
+import game_functions as gf
+from settings import Settings
+from ship import Ship
+
+def run_game():
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+    ship = Ship(screen)
+
+    while True:
+        gf.check_events()
+        gf.update_screen(ai_settings, screen, ship)
+
+run_game()
