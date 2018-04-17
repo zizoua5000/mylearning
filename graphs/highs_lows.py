442fe2414705588a6add5f46637757f18149da87
zizoua5000 <zizoua5000@gmail.com>
2018-04-17 17:52:57 +0400

    page 259

--- graphs/highs_lows.py
+++ graphs/highs_lows.py
@@ -25,8 +25,9 @@ with open(filename) as f:
 
 # plot data
 fig = plt.figure(dpi=128, figsize=(10, 6))
-plt.plot(dates, highs, c='red')
-plt.plot(dates, lows, c="blue")
+plt.plot(dates, highs, c='red', alpha=0.5)
+plt.plot(dates, lows, c="blue", alpha=0.5)
+plt.fill_between(dates, highs, lows, facecolor="blue", alpha=0.1)
 
 # Format plot
 plt.title("Daily high temperatures, 2014", fontsize=14)
